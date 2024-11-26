ce6af6917befbef53f6092de2d15c4247e874756
LucaIgnatescu <lucaignatescu003@gmail.com>
2024-11-26 14:51:54 -0500

    added testing and publishing workflows

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET')  
@@ -8,6 +9,7 @@ mongodb_username = os.getenv('MONGODB_USERNAME')
 mongodb_password = os.getenv('MONGODB_PASSWORD')
 
 db_uri = f"mongodb://{mongodb_username}:{mongodb_password}@mongodb:27017"
+client = MongoClient(db_uri)
 
 @app.route('/')
 def index():
