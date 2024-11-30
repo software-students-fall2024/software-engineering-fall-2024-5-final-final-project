2c1254aabe790e7d404bdcde7c4008bda3b3d29c
ntb5562 <ntb5562@nyu.edu>
2024-11-30 16:16:30 -0500

    setting up css and localhost

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import os
 from flask import Flask, render_template
 from pymongo import MongoClient
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET')  
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET')  
 
-mongodb_username = os.getenv('MONGODB_USERNAME') 
-mongodb_password = os.getenv('MONGODB_PASSWORD')
+    mongodb_username = os.getenv('MONGODB_USERNAME') 
+    mongodb_password = os.getenv('MONGODB_PASSWORD')
 
-db_uri = f"mongodb://{mongodb_username}:{mongodb_password}@mongodb:27017"
-client = MongoClient(db_uri)
+    db_uri = f"mongodb://{mongodb_username}:{mongodb_password}@mongodb:27017"
+    client = MongoClient(db_uri)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    return app
+
+if __name__ == "__main__":
+    FLASK_PORT = os.getenv("FLASK_PORT", "8080")
+    app = create_app()
+    app.run(port=FLASK_PORT)
