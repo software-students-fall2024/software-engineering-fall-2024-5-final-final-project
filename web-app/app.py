3d06a75d51ca52b0385229e5b5df60006ec9ebec
seeyeh <will.yn.xie@gmail.com>
2024-12-03 22:38:29 -0500

    App.py storing csv data into variable that can be accessed by all routes

--- web-app/app.py
+++ web-app/app.py
@@ -1,10 +1,22 @@
-from flask import Flask;
+from flask import Flask, g;
+import csv;
+import os;
 
 app = Flask(__name__)
 
+# Runs before requests - reads csv file and puts each row into all_movies variable
+@app.before_request
+def read_movies():
+    g.all_movies = []
+    with open(os.path.join(os.path.dirname(__file__), 'input/imdb_top_1000.csv'), 'r') as file:
+       csv_reader = csv.reader(file)
+       for row in csv_reader:
+          g.all_movies.append(row)
 
 @app.route('/')
-def hello_world():
+def index():
+    for row in g.all_movies:
+        print(row[1])
     return 'Hello World'
 
 if __name__ == '__main__':
