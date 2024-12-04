51ff5fc37db07db6baa4f4ad2b72807325201eb7
favils <fav2019@nyu.edu>
2024-12-04 15:19:50 -0500

    app initialized

--- app/app.py
+++ app/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from flask_login import UserMixin
 
-app = Flask(__name__)
+def create_app():
 
-@app.route('/')
-def home():
-    return render_template('index.html')  # Renders templates/index.html
+    app = Flask(__name__)
+    client = MongoClient("mongodb://mongodb:27017/")
 
-@app.route('/resume')
-def resume():
-    return
+    db = client.resume_db
+
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/login", methods = ['GET', 'POST'])
+    def login():
+        return render_template("login.html")
+    
+    @app.route("/register", methods = ['POST', 'GET'])
+    def register():
+        return render_template("register.html")
+    
+    return app
+
+if __name__ == "__main__":
+    create_app().run(host="0.0.0.0", port=5002, debug=True)
