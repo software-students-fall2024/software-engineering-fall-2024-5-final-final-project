38955458b31632041484312117f75936e9ae68ce
reyhanquayum <reyhan.quayum@gmail.com>
2024-12-05 16:23:22 -0500

    login user page aut

--- web-app/app.py
+++ web-app/app.py
@@ -7,25 +7,24 @@ from flask_login import current_user, login_required
 from user.auth import auth, login_manager
 
 app = Flask(__name__)
-app.secret_key = ""
+app.secret_key = "secret_key" # needed for flask login sessions
 
 login_manager.init_app(app)
-app.register_blueprint(auth, url_prefix="/auth")  # routes that need auth prefixed with /auth
-
+app.register_blueprint(auth)
 
 @app.route("/")
 def index():
     """Redirect to right page if logged in or not"""
     if current_user.is_authenticated:
-        return redirect(url_for("home"))
-    return redirect(url_for("auth.login"))
+        return render_template("calendar.html")
+    return render_template("login.html")
 
 
 @app.route("/")
 @login_required
 def home():
     """Render main page"""
-    return render_template("index.html")
+    return render_template("calendar.html")
 
 
 # write new functions here
