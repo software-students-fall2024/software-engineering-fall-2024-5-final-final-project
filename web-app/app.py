3de7ebfba4fef2176f8a5a2dc02ca694aa8e83c6
jh7316 <jh7316@nyu.edu>
2024-12-05 14:42:17 -0500

    Set up routes for loading pages

--- web-app/app.py
+++ web-app/app.py
@@ -7,10 +7,35 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# @app.route("/")
+# def home():
+#     """Render main page"""
+#     return render_template("index.html")
+
 @app.route("/")
-def home():
-    """Render main page"""
-    return render_template("main.html")
+def login():
+    """Render login page"""
+    return render_template("Login.html")
+
+@app.route("/signup")
+def signup():
+    """Render Signup page"""
+    return render_template("Signup.html")
+
+@app.route("/menu")
+def menu():
+    """Render menu page"""
+    return render_template("Menu.html")
+
+@app.route("/calendar")
+def calendar():
+    """Render calendar page"""
+    return render_template("Calendar.html")
+
+@app.route("/analytics")
+def analytics():
+    """Render Analytics page"""
+    return render_template("Analytics.html")
 
 
 # write new functions here
