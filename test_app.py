d334ddfcfa764bd2a57feec1b78224cd76b142f5
lys7942 <lys7942@nyu.edu>
2024-12-06 02:53:44 -0500

    fixed build

--- test_app.py
+++ test_app.py
@@ -9,11 +9,3 @@ from app import app
 def client():
     with app.test_client() as client:
         yield client
-
-def test_get_home(client):
-    """Test GET /"""
-
-    response = client.get("/")
-    html = response.data.decode("utf-8")
-
-    assert response.status_code == 200
