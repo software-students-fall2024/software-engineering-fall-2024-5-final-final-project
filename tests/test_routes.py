d49d3e097daf87785dc14f1ea10239032ba42074
dangelo729 <peterdangelo729@gmail.com>
2024-12-05 21:17:20 -0500

    reformat

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,23 +2,23 @@ import pytest
 from unittest.mock import MagicMock, patch
 from backend import create_app
 
-#TODO: Fix the test
+# TODO: Fix the test
 
 # @pytest.fixture
 # def client():
 #     """Create the test client for Flask app with mocked Database."""
 #     with patch('backend.routes.Database') as MockDatabase:
 #         mock_db = MagicMock()
-        
+
 #         # Mock the DB
 #         mock_db.get_user_data.return_value = {
 #             "username": "user",
 #             "budget": 1000.0,
 #             "total_expenses": 200.0
 #         }
-        
+
 #         mock_db.get_expenses.return_value = []
-        
+
 #         MockDatabase.return_value = mock_db
 
 #         app = create_app()
