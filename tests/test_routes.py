0cd00036a9ba8322d7d5f3ed2e9713f094c7204e
dangelo729 <peterdangelo729@gmail.com>
2024-12-05 21:45:18 -0500

    take 14

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,30 +1,7 @@
-import pytest
-
-# from backend import create_app
+import pytest  # type: ignore
 
 
-
-
-def test_dummy():
-    """ fake test for now"""
-    assert True
-
-
-# @pytest.mark.skip(
-#     reason="Temporarily disabling tests due to database connection issues"
-# )
-# def client():
-#     """Create the test client"""
-#     app = create_app()
-#     app.testing = True
-#     with app.test_client() as client:
-#         yield client
-
-
-# @pytest.mark.skip(
-#     reason="Temporarily disabling tests due to database connection issues"
-# )
-# def test_index(client):
-#     """Test the index route."""
-#     response = client.get("/")
-#     assert response.status_code == 200
+def test_index(client):
+    """Test the index route."""
+    response = client.get("/")
+    assert response.status_code == 200
