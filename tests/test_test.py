6b1709ad3809dbe2c508c8825847d96fc20833c0
nehamagesh <neha3magesh@gmail.com>
2024-12-05 19:29:49 -0500

    test 1

--- tests/test_test.py
+++ tests/test_test.py
@@ -4,3 +4,24 @@ from app import app
 
 def test_secret():
     assert app.config["SECRET_KEY"] is not None
+
+
+@pytest.fixture
+def test_client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_mock_db(monkeypatch, test_client):
+    class MockMongoClient:
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def __getattr__(self, name):
+            return self
+
+    monkeypatch.setattr("pymongo.MongoClient", MockMongoClient)
+
+    response = test_client.get("/")
+    assert response.status_code == 200
