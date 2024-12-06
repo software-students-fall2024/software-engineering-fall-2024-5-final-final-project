5dd2c3672992254e3eb02b2b6484956a62a89742
Maddy Li <md5013@nyu.edu>
2024-12-06 15:29:04 -0500

    home page template

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from __init__ import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=True)
