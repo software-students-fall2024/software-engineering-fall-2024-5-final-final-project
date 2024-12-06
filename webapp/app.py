be90f270da525e4daeb296c328f8c7b181269437
BringoJr <hwb4547@nyu.edu>
2024-12-05 21:49:46 -0500

    basic webapp structure

--- webapp/app.py
+++ webapp/app.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-    
+
+@app.route('/groups')
+def groups():
+    return render_template('groups.html')
+
+@app.route('/add-expense')
+def add_expense():
+    return render_template('add-expense.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
---
+++ webapp/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+    return render_template('groups.html')
