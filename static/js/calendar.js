d3adf3e001e248abb906824fd80d02281858ea51
lys7942 <lys7942@nyu.edu>
2024-12-07 19:31:30 -0500

    fixed bugs on db calendar relation

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,16 +1,4 @@
-const popup = document.getElementById('popup');
-const delBtn = document.getElementById('deleteBtn');
-const closeBtn = document.getElementById('closeBtn');
-const todayEvents = document.getElementById('todayEvents');
-
-const title = document.getElementById('event-title');
-const startTime = document.getElementById('event-startTime');
-const endTime = document.getElementById('event-endTime');
-const reminder = document.getElementById('checkbox');
-
 let curEvent = '';
-let curReminder = false;
-
 const req = new XMLHttpRequest();
 
 document.addEventListener('DOMContentLoaded', async function() {
@@ -20,7 +8,8 @@ document.addEventListener('DOMContentLoaded', async function() {
 
     const eventList = [];
     data.forEach(function(msg) {
-        eventList.push({title: msg.name, start: msg.time.$date, /**end: msg.time,*/ extendedProps: {id: msg._id}});
+        console.log(typeof msg.time)
+        eventList.push({title: msg.name, start: msg.time, extendedProps: {id: msg._id}});
     });
 
     console.log(eventList)
@@ -36,36 +25,16 @@ document.addEventListener('DOMContentLoaded', async function() {
         editable: true,
         dayMaxEvents: true,
         eventClick: function(info) {
-            title.innerHTML = info.event.title;
-            startTime.innerHTML = new Date(info.event.start).toLocaleString();
-            if (info.event.end != '') endTime.innerHTML = new Date(info.event.start).toLocaleString();
-            curEvent = info.event;
-
-            info.el.parentNode.appendChild(popup);
-            popup.style.display = 'inline-block';
+            window.location.href = 'http://localhost:3000/event/' +  info.event.extendedProps.id.$oid+'/edit';
         },
         eventChange: function(info) {
-            req.open('POST', '/event/'+info.event.extendedProps.id.$oid+'/edit', true)
+            req.open('POST', '/event/'+info.event.extendedProps.id.$oid+'/time', true)
             req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-            console.log(info.event.start)
-            req.send(`start=${encodeURIComponent(info.event.start)}`);
-        },
+            console.log(info.event.start.toISOString())
 
+            req.send(`start=${encodeURIComponent(info.event.start.toISOString())}`);
+        },
+        eventDurationEditable: false,
     });
     calendar.render();
 }); 
-
-delBtn.addEventListener('click', function() {
-    req.open('POST', '/event/'+curEvent.extendedProps.id+'/delete', true)
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    req.send();   
-    curEvent.remove();
-    popup.style.display = 'none';
-});
-
-closeBtn.addEventListener('click', function() {
-    popup.style.display = 'none' ;
-    req.open('POST', '/event/'+curEvent.extendedProps.id+'/edit', true)
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    req.send('reminder='+reminder.checked);       
-});
