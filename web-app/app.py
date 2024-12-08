155cd3ae6a46c3afb56f995c62aa877d6d8b1f59
seeyeh <will.yn.xie@gmail.com>
2024-12-08 18:29:51 -0500

    Now checks if a User has assigned movie for the day and if not, assigns one. Web page displays whatever is the User's assigned movie for the dya

--- web-app/app.py
+++ web-app/app.py
@@ -5,18 +5,19 @@ import random;
 import os
 from dotenv import load_dotenv
 from pymongo import MongoClient, server_api
-import certifi
+import certifi, datetime
 
 
 load_dotenv()
 
 MONGO_URI = os.getenv('MONGO_URI')
+db_name = os.getenv('MONGO_DBNAME')
 
 app = Flask(__name__)
 
 app.config["MONGO_URI"] = MONGO_URI
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where(), server_api=server_api.ServerApi('1'))
-db = client["movie_db"]
+db = client[db_name]
 users_collection = db.users
 # mongo = PyMongo(app)
 
@@ -24,9 +25,21 @@ def create_user(username, password):
     user = {
         "username": username,
         "password": password,  # Maybe hash this?
+        "daily_movie":{
+            "movie_id": random.randint(1,1000),
+            "recommended_date": datetime.datetime.now()
+        },
+        "watched_movies": []
     }
     return user
 
+def random_movie_id(watched_ids):
+    id = 0;
+    while True:
+        id = random.randint(1,1000)
+        if id not in watched_ids:
+            break
+    return id
 
 # Runs before requests - reads csv file and puts each row into all_movies variable
 @app.before_request
@@ -39,10 +52,35 @@ def read_movies():
 
 @app.route('/')
 def index():
-    user = create_user('WilliamTest','unhashedpass')
-    users_collection.insert_one(user);
+
+    # user = create_user('WilliamTest2','unhashedpass')
+    # users_collection.insert_one(user);
     # mongo.db.users.insert_one(user)
-    movie_picked = False
+
+    selectedUser = users_collection.find_one({"username":"WilliamTest2"})
+
+    if selectedUser:
+
+        # print("User's date: " + selectedUser["daily_movie"]["recommended_date"].strftime("%Y %m %d"))
+        # print("Computer's date: " + datetime.datetime.now().strftime("%Y %m %d"))
+        # print("Equal?: "+ str(selectedUser["daily_movie"]["recommended_date"] == datetime.datetime.now()))
+
+        # Checks if movie has been assigned for the day if last recommended movie date is same as today
+        user_movie_id = selectedUser["daily_movie"]["movie_id"]
+        user_recommended_date = selectedUser["daily_movie"]["recommended_date"]
+        
+        isAlreadyAssigned = user_recommended_date.strftime("%Y %m %d") == datetime.datetime.now().strftime("%Y %m %d")
+
+        # if movie hasn't been assigned for the day, set a new movie id user hasn't seen before
+        if not isAlreadyAssigned:
+            new_movie_id = random_movie_id(selectedUser["watched_movies"])
+            user_movie_id = new_movie_id
+            user_recommended_date = datetime.datetime.now()
+
+        selected_movie = g.all_movies[user_movie_id]
+
+    movie_picked = True
+
     if not movie_picked:
         selected_movie = g.all_movies[random.randint(1,1000)]
     # for row in g.all_movies:
