7590f7743be82621e1bd48b97b95e02298a69d82
reeseburns <157714070+reeseburns@users.noreply.github.com>
2024-12-08 01:18:02 -0500

    Add files via upload
    Updated web-app and bar-recs files

--- web-app/static/script.js
+++ web-app/static/script.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector(".navbar");
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
+// Direct to clicked tab
 window.onscroll = () => {
     selections.forEeach(sec => {
         let top = window.scrollY;
@@ -32,3 +33,12 @@ document.getElementById('menu-icon').addEventListener('click', function() {
         navbar.style.display = 'block';
     }
 });
+
+// "Action" cell in table
+function handleAction(selectElement, barId) {
+    const action = selectElement.value; // get selected action
+
+    if (action === "add") {window.location.href = "/add";}                     // direct to add.html 
+    else if (action === "edit") {window.location.href = `/edit/${barId}`;}     // direct to add.html 
+    else if (action === "delete") {window.location.href = `/delete/${barId}`;} // direct to add.html
+}
