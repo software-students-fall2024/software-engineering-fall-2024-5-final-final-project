60abb13ddcbd2ff1f87dff0ff523ae2ad63d1339
drj8812 <drj8812@nyu.edu>
2024-12-09 01:12:38 -0500

    working with docker and mongo now

--- flask-app/app.py
+++ flask-app/app.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify
 import flask_login
 from flask_login import login_user, logout_user, login_required
 from craps_func import linebet, buybet
+import db
 
 # instantiate flask app, create key
 app = Flask(__name__)
@@ -19,8 +20,10 @@ login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
 # simulate database of users, placeholder until pymongo connection
-users = {'abc': {'password': 'xyz'},
-         'zyx': {'password': 'cba'}}
+db.register_user("abc", "xyz")
+users = db.get_users()
+# users = {}
+# users["abc"] = {"password": "xyz"}
 
 users_balance = {'abc': 500,
                  'zyx': 900}
@@ -28,6 +31,7 @@ users_balance = {'abc': 500,
 # create User object
 class User(flask_login.UserMixin):
     pass
+
 
 @login_manager.user_loader
 def user_loader(username):
@@ -78,7 +82,7 @@ def user_home(username):
     # if username in users_balance:
     #     user_balance = users_balance[username]['balance']
     #     return render_template('user_home.html', username=username, balance = user_balance)
-    user_balance = users_balance[str(username)]
+    user_balance = db.get_bal(username)
     return render_template('user_home.html', username=username, balance = user_balance)
     
 @app.route("/<username>/craps", methods = ["POST", "GET"])
@@ -100,14 +104,18 @@ def playline(username):
     for r in rolls:
         return_text+=r + "@"
     
+    win = False
     if result == 'w':
         return_text += "You win!"
         balance += bet
+        win = True
     elif result == 'l':
         return_text+= "You lost!"
         balance-=bet 
     else:
         return_text += "It was a tie!"
+
+    db.update_bal(username, balance, win, "craps")
 
     # update database maybe use requests?
     # or maybe make a different .py that will make a request
@@ -124,14 +132,19 @@ def playbuy(username):
     balance = float(request.form["balance"])
     rolls, result, odds = buybet(win, place)
     return_text= ""
+    win = False
     for r in rolls:
         return_text+=r + "@"
     if result == 'w':
         return_text += "You win!"
         balance += bet * odds
+        win = True
     elif result == 'l':
         return_text+= "You lost!"
         balance-=bet 
+
+    db.update_bal(username, balance, win, "craps")
+    
     return render_template("craps_results.html", username=username, balance=balance, textResult=return_text)
 
 
@@ -147,4 +160,4 @@ def unauthorized_handler():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
