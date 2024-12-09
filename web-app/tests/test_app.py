6fda73d8f049969931f66087272ae0ba5d6247f2
jljune9li <jljune9li@gmail.com>
2024-12-09 03:54:12 -0500

    save

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -17,40 +17,40 @@ def client():
         yield client
 
 
-# @patch('app.requests.get')
-# def test_get_weather_success(mock_get):
-#     """Test successful weather data retrieval."""
-#     mock_response = MagicMock()
-#     mock_response.status_code = 200
-#     mock_response.json.return_value = {
-#         'main': {'temp': 25},
-#         'weather': [{'description': 'clear sky'}]
-#     }
-#     mock_get.return_value = mock_response
+@patch('app.requests.get')
+def test_get_weather_success(mock_get):
+    """Test successful weather data retrieval."""
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        'main': {'temp': 25},
+        'weather': [{'description': 'clear sky'}]
+    }
+    mock_get.return_value = mock_response
 
-#     temperature, description = get_weather('New York', 'test_api_key')
-#     assert temperature == 25
-#     assert description == 'clear sky'
-
-
-# @patch('app.requests.get')
-# def test_get_weather_failure(mock_get):
-#     """Test failed weather data retrieval."""
-#     mock_response = MagicMock()
-#     mock_response.status_code = 404
-#     mock_get.return_value = mock_response
-
-#     temperature, description = get_weather('InvalidCity', 'test_api_key')
-#     assert temperature is None
-#     assert description is None
+    temperature, description = get_weather('New York', 'test_api_key')
+    assert temperature == 25
+    assert description == 'clear sky'
 
 
-# @patch('app.db.users.find_one', return_value={
-#     "_id": ObjectId(),
-#     "username": "testuser",
-#     "password": "hashedpassword",
-#     "gender": "male"
-# })
+@patch('app.requests.get')
+def test_get_weather_failure(mock_get):
+    """Test failed weather data retrieval."""
+    mock_response = MagicMock()
+    mock_response.status_code = 404
+    mock_get.return_value = mock_response
+
+    temperature, description = get_weather('InvalidCity', 'test_api_key')
+    assert temperature is None
+    assert description is None
+
+
+@patch('app.db.users.find_one', return_value={
+    "_id": ObjectId(),
+    "username": "testuser",
+    "password": "hashedpassword",
+    "gender": "male"
+})
 # @patch('app.check_password_hash', return_value=True)
 # def test_login_success(mock_hash, mock_find_one, client):
 #     """Test successful login."""
